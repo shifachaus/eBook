16db8c0b0a6ae9c1f13a92ffdb92507e539b7d0c
shifachaus <chausshifa804@gmail.com>
2024-04-23 18:35:42 +0530

    add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Discover, Connect, and Explore Worlds Between Pages.</h1>;
+  return (
+    <>
+      <h1>Discover, Connect, and Explore Worlds Between Pages.</h1>
+    </>
+  );
 }
